10ebadf5705d280e9aa7d5f4e006c9933c511dfc
Adedejiabdul16 <olaiyaabdulazeem16@gmail.com>
2026-07-31 21:44:08 +0100

    Add root endpoint in main

--- app/main.py
+++ app/main.py
@@ -24,3 +24,9 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
+
+
+
+@app.get('/')
+def root():
+    return {'message' : 'Hi there!'}
